35b1fc4abb0a24afa0fd5cef14d2fedb5c79be44
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-05-08 08:36:51 -0500

    Move uuid creation from Book to addBook function

--- library-project.js
+++ library-project.js
@@ -4,7 +4,7 @@ function Book (title, author, pages, status) {
     if(!new.target) {
         throw Error("Must use New operator");
     }
-    
+
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -13,13 +13,12 @@ function Book (title, author, pages, status) {
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}.`;
     }
-
-    this.id = crypto.randomUUID();
 };
 
 function addBookToLibrary(title, author, pages, status) {
     const libraryBook = new Book(title, author, pages, status); // Pull constructor properties
     myLibrary.push(libraryBook); // send new Book to our array
+    this.id = crypto.randomUUID(); // Creates unique ID on creation of book
 }
 
 addBookToLibrary("Eragon", "Christoper Paolini", 509, "read");
