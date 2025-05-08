d4f7bcf17bbd4b353b22733e16d90e1b05f7106a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-05-08 08:41:59 -0500

    Change read status to a falsy value

--- library-project.js
+++ library-project.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book (title, author, pages, status) {
+function Book (title, author, pages, isRead) {
     if(!new.target) {
         throw Error("Must use New operator");
     }
@@ -8,19 +8,19 @@ function Book (title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
+    this.isRead = isRead;
 
     this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}.`;
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}.`;
     }
 };
 
-function addBookToLibrary(title, author, pages, status) {
-    const libraryBook = new Book(title, author, pages, status); // Pull constructor properties
+function addBookToLibrary(title, author, pages, isRead) {
+    const libraryBook = new Book(title, author, pages, isRead); // Pull constructor properties
     myLibrary.push(libraryBook); // send new Book to our array
     this.id = crypto.randomUUID(); // Creates unique ID on creation of book
 }
 
-addBookToLibrary("Eragon", "Christoper Paolini", 509, "read");
+addBookToLibrary("Eragon", "Christoper Paolini", 509, true);
 
 console.log(myLibrary);
